d2547649d3246dba48f7c12d7251543bf015ea61
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-02-16 11:46:56 +0530

    feat: Establish application root layout, integrate PWA configuration, and implement basic authentication.

--- app/root.tsx
+++ app/root.tsx
@@ -132,22 +132,6 @@ export default function App() {
 
   useEffect(() => {
     refreshAuth();
-
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js").then(
-          (registration) => {
-            console.log(
-              "ServiceWorker registration successful with scope: ",
-              registration.scope,
-            );
-          },
-          (err) => {
-            console.log("ServiceWorker registration failed: ", err);
-          },
-        );
-      });
-    }
   }, []);
 
   const signIn = async () => {
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,36 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Roomify",
+        short_name: "Roomify",
+        theme_color: "#f97316",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
